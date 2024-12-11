48a93c2533b712d4ff3af67cc5191339cc7a3074
jeev86399 <nnani86399@gmail.com>
2024-12-11 15:37:24 +0530

    Add files via upload

---
+++ pow8.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class pow8{
+    static int pow(int b,int ex){
+        if(ex==0)return 1;
+        return b*pow(b,ex-1);
+    }
+    public Static void main(String args[]){
+        int res=pow(7,6);
+        System.out.println(res);
+    }
+}
