10ee73d9f8ba8e8cbdecbab5e9ec05c2e72b7c5e
jeev86399 <nnani86399@gmail.com>
2024-12-11 15:36:05 +0530

    Add files via upload

---
+++ Class1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Class1{
+    public static void main(String args[]){
+        a r=new a();
+        System.out.println(r.a1);
+    }
+}
+class a{
+    int a1=5;
+}
