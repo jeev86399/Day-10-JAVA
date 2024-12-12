726642dc2c28e399f149ec2c3a056f66bbbc6f25
jeev86399 <nnani86399@gmail.com>
2024-12-12 10:58:39 +0530

    Add files via upload

---
+++ Staticnonvoid3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Staticnonvoid3{
+    public static String s(){
+        return "STATIC WITH NON VOID METHOD";
+    }
+    public static void main(String args[]){
+        String b=s();
+        System.out.println(b);
+    }
+}
